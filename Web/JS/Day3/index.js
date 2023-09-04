ee1691d9e92d45d5d5588a30dc16b6917e59f7c5
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2023-09-04 12:26:06 +0530

    updated

---
+++ Web/JS/Day3/index.js
@@ -0,0 +1,21 @@
+// let x=5; //accessible inside a scope
+// var y=10;//globally accessible
+// console.log(y);
+// const z=20;//can't be changed
+// //memory  allocation
+// //code execution
+// y=10;
+// var y;
+// console.log(y);
+let isInvalid=true;
+console.log(isInvalid);
+let ob={msg:"hello",k:"5", "5/5":"Aw"}; //object
+console.log(ob);
+console.log(ob.msg);
+console.log(ob["msg"]);
+console.log(ob["5/5"]); //to access complex opertors 
+// let arr=[5,"s"];
+// let arr2={
+//     [5,2]:
+//     ["s","y"]
+// }
