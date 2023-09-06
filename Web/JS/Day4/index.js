abb4a5b868adb6c5720662ea638f9988bbcf42a7
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2023-09-06 13:57:50 +0530

    updated

--- Web/JS/Day4/index.js
+++ Web/JS/Day4/index.js
@@ -39,12 +39,65 @@
 // var x;
 
 
-var x=7;
-getName();
-console.log(getName);
-function getName(){
-    console.log("Namaste JS");
-}
+// var x=7;
+// getName();
+// console.log(getName);
+// function getName(){
+//     console.log("Namaste JS");
+// }
 
 
-console.log(x);
+// console.log(x);
+
+var d=document.getElementById("elm");
+// var a=document.getElementsByClassName("elm2");
+console.log(d);
+d.innerHTML="Hello World";
+// console.log(a);
+var click=false;
+function clickMe() { 
+    if(!click){
+    document.getElementById("elm").style.color="red";
+    document.getElementById("elm").style.fontFamily="sans-serif";
+    document.getElementById("elm").style.fontSize="10px";
+    click=true;
+    } else{
+        document.getElementById("elm").style.color="black";
+    document.getElementById("elm").style.fontFamily="sans-serif";
+    document.getElementById("elm").style.fontSize="15px";
+    click=false;
+    }
+ }
+
+ var p=document.getElementsByTagName('p');
+
+
+ for(var i=0;i<p.length;i++){
+    p[i].style.color="blue";
+ }
+
+ console.log(p);
+
+ function alerttt(){
+    alert("onkeydown");
+ }
+
+ function onMouseOverPara(){
+    var d=document.getElementById("para");
+    d.style.color="green";
+    d.style.fontSize="40px";
+
+ }
+
+ document.getElementById("tryit").addEventListener("click",displayDate);
+
+ function displayDate(){
+    document.getElementById("demo").innerHTML= Date();
+    console.log("clicked");
+    document.getElementById("tryit").removeEventListener("click",displayDate);
+    document.getElementById("tryit").addEventListener("click",myFun);
+ }
+
+ function myFun(){
+    alert("Hello World");
+ }
